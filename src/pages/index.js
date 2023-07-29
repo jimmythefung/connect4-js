5164f837a14e336557cf17214f44a4f05997df0c
Jimmy Fung <jimmythefung@yahoo.com>
2023-07-29 15:42:05 -0400

    workign nextjs start

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Connect4</title>
+                <meta name="description" content="Connect4 Game" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main>
+                <p>Hello World</p>
+            </main>
+        </>
+    );
+}
