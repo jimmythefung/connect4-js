92372cbc3884819e110d72384036160a1019898a
Jimmy Fung <jimmythefung@yahoo.com>
2023-08-02 21:12:06 -0400

    game tracks current board

--- src/components/board.js
+++ src/components/board.js
@@ -1,6 +1,9 @@
 import styles from "./game.module.css";
 
-export default function Board({ m_rows, n_columns, onColumnClick }) {
+export default function Board({ current_board, onCellClick }) {
+    let m_rows = current_board.length;
+    let n_columns = current_board[0].length;
+
     // Build up a board made of columns
     let full_board = [];
     for (let c = 0; c < n_columns; c++) {
@@ -8,16 +11,20 @@ export default function Board({ m_rows, n_columns, onColumnClick }) {
         let a_column = [];
         for (let r = 0; r < m_rows; r++) {
             let index = "(" + r.toString() + ", " + c.toString() + ")";
-            a_column.push(<Cell key={index} text={index} row={r} col={c} />);
+            a_column.push(
+                <Cell
+                    key={index}
+                    text={current_board[r][c] + ": " + index}
+                    row={r}
+                    col={c}
+                    clickHandler={onCellClick}
+                />
+            );
         }
 
         // Add column to board
         full_board.push(
-            <div
-                key={c}
-                className={`${styles.column}`}
-                onMouseEnter={() => onColumnClick(c)}
-            >
+            <div key={c} className={`${styles.column}`}>
                 {a_column}
             </div>
         );
@@ -25,11 +32,12 @@ export default function Board({ m_rows, n_columns, onColumnClick }) {
     return <div className={`${styles.board}`}>{full_board}</div>;
 }
 
-function Cell({ text, row, col }) {
+function Cell({ text, row, col, clickHandler }) {
     return (
         <div
             className={`${styles.cell}`}
             onClick={() => console.log("Clicked " + text)}
+            onMouseEnter={() => clickHandler(row, col)}
         >
             {text}
         </div>
--- src/components/game.js
+++ src/components/game.js
@@ -6,8 +6,15 @@ export default function Game() {
     const [col_select, setColSelect] = useState(0);
     let [m, n] = [6, 7];
 
-    function handleColClick(column) {
+    let empty_board = Array.from(Array(m), () => new Array(n).fill(0));
+    const [currentBoard, setCurrentBoard] = useState(empty_board);
+
+    function handleCellClick(row, column) {
         setColSelect(column);
+
+
+        currentBoard[row][column] = currentBoard[row][column] + 1;
+        setCurrentBoard(currentBoard);
     }
 
     return (
@@ -15,7 +22,7 @@ export default function Game() {
             <div className={`${styles.game}`}>
                 <div className={`${styles["game-div"]}`}>Connect 4 Game!</div>
                 <div className={`${styles["game-div"]}`}>
-                    <Board m_rows={m} n_columns={n} onColumnClick={handleColClick} />
+                    <Board current_board={currentBoard} onCellClick={handleCellClick} />
                 </div>
                 <div className={`${styles["game-div"]}`}>Select column: {col_select}</div>
             </div>
