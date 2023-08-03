abc7f154b5ae04d5a63836a731d6692b4bd77860
Jimmy Fung <jimmythefung@yahoo.com>
2023-08-03 17:50:46 -0400

    checks winner

--- src/components/game.js
+++ src/components/game.js
@@ -14,6 +14,120 @@ export default function Game() {
     const [turn, setTurn] = useState(PLAYER2);
     const is_p1_turn = turn === PLAYER1;
 
+    // Check winner on board re-render
+    useEffect(() => {
+        let symbol = turn == PLAYER1 ? PLAYER2 : PLAYER1;
+        if (check_winner(currentBoard, symbol)) {
+            console.log("We have a winner!");
+            alert(symbol + " is the winner!");
+        }
+    }, [currentBoard]);
+
+    // Helpers
+    function scan_direction(board, r, c, l, direction, symbol) {
+        let [m, n] = [board.length, board[0].length];
+        // Base case: boundary check
+        if (r > m - 1 || r < 0) {
+            return 0;
+        }
+        if (c > n - 1 || c < 0) {
+            return 0;
+        }
+        if (l === 0) {
+            return 0;
+        }
+        if (direction === "v") {
+            if (board[r][c] === symbol) {
+                return (
+                    1 +
+                    scan_direction(board, r + 1, c, l - 1, direction, symbol)
+                );
+            } else {
+                return 0;
+            }
+        }
+        if (direction === "h") {
+            if (board[r][c] === symbol) {
+                return (
+                    1 +
+                    scan_direction(board, r, c + 1, l - 1, direction, symbol)
+                );
+            } else {
+                return 0;
+            }
+        }
+        if (direction === "dl") {
+            if (board[r][c] === symbol) {
+                return (
+                    1 +
+                    scan_direction(
+                        board,
+                        r + 1,
+                        c - 1,
+                        l - 1,
+                        direction,
+                        symbol
+                    )
+                );
+            } else {
+                return 0;
+            }
+        }
+        if (direction === "dr") {
+            if (board[r][c] === symbol) {
+                return (
+                    1 +
+                    scan_direction(
+                        board,
+                        r + 1,
+                        c + 1,
+                        l - 1,
+                        direction,
+                        symbol
+                    )
+                );
+            } else {
+                return 0;
+            }
+        }
+    }
+
+    function check_winner_helper(board, r, c, symbol) {
+        const win_length = 4;
+        // const symbol = board[r][c];
+        let h = scan_direction(board, r, c, win_length, "h", symbol);
+        if (h === win_length) {
+            return true;
+        }
+        let v = scan_direction(board, r, c, win_length, "v", symbol);
+        if (v === win_length) {
+            return true;
+        }
+        let dl = scan_direction(board, r, c, win_length, "dl", symbol);
+        if (dl === win_length) {
+            return true;
+        }
+        let dr = scan_direction(board, r, c, win_length, "dr", symbol);
+        if (dr === win_length) {
+            return true;
+        }
+        return false;
+    }
+
+    function check_winner(board, symbol) {
+        let [m, n] = [board.length, board[0].length];
+        let hasWinner = false;
+        for (let r = 0; r < m; r++) {
+            for (let c = 0; c < n; c++) {
+                hasWinner = check_winner_helper(board, r, c, symbol);
+                if (hasWinner) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     function handleCellClick(row, column) {
         // meta
         setColSelect(column);
@@ -22,41 +136,16 @@ export default function Game() {
         // Drop the token to the bottom at the given column
         // Note: slice is neccessary to trigger react to refresh/update the board
         let newBoard = currentBoard.slice();
+        let symbol = is_p1_turn ? PLAYER1 : PLAYER2;
         for (let r = newBoard.length - 1; r >= 0; r--) {
             if (newBoard[r][column] === BLANK) {
                 // Place the player's token based on the turn
-                newBoard[r][column] = is_p1_turn ? PLAYER1 : PLAYER2;
+                newBoard[r][column] = symbol;
                 setTurn(is_p1_turn ? PLAYER2 : PLAYER1);
                 setCurrentBoard(newBoard);
-
-                // meta
-                console.log("Updated column: " + column + " at row: " + r);
                 return;
             }
         }
-
-        // // Drop the token to the bottom at the given column
-        // // Note: slice is neccessary to trigger react to refresh/update the board
-        // let newBoard = currentBoard.slice();
-        // let symbol = is_p1_turn ? PLAYER1 : PLAYER2;
-        // for (let r = 0; r < newBoard.length; r++) {
-        //     if (newBoard[r][column] === BLANK) {
-        //         // Place the player's token based on the turn
-        //         newBoard[r][column] = symbol;
-        //         setCurrentBoard(newBoard);
-
-        //         // Clear the animation
-        //         useEffect(() => {
-        //             setTimeout(() => {
-        //                 if (r > 0) {
-        //                     newBoard[r - 1][column] = BLANK;
-        //                     setCurrentBoard(newBoard);
-        //                 }
-        //             }, 500);
-        //         }, [currentBoard]);
-        //     }
-        // }
-        // setTurn(is_p1_turn ? PLAYER2 : PLAYER1);
     }
 
     return (
