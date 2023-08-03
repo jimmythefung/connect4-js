05e167b19f815b907123b92b24541fe5f8582ba5
Jimmy Fung <jimmythefung@yahoo.com>
2023-08-03 03:24:03 -0400

    2 players turn toggle

--- src/components/game.js
+++ src/components/game.js
@@ -5,26 +5,59 @@ import { useState } from "react";
 export default function Game() {
     const [col_select, setColSelect] = useState(0);
     let [m, n] = [6, 7];
-    let BLANK = '';
+    let BLANK = "";
+    let PLAYER1 = "X";
+    let PLAYER2 = "O";
 
     let empty_board = Array.from(Array(m), () => new Array(n).fill(BLANK));
     const [currentBoard, setCurrentBoard] = useState(empty_board);
+    const [turn, setTurn] = useState(PLAYER2);
+    const is_p1_turn = turn === PLAYER1;
 
-    function handleCellClick(row, column) {
+    let sleep = (ms) => {
+        return new Promise((resolve) => setTimeout(resolve, ms));
+    };
+
+    async function handleCellClick(row, column) {
         // meta
         setColSelect(column);
         console.log("Cell " + [row, column] + " clicked.");
-
+        
         // Drop the token to the bottom at the given column
+        // Note: slice is neccessary to trigger react to refresh/update the board
         let newBoard = currentBoard.slice();
         for (let r = newBoard.length - 1; r >= 0; r--) {
             if (newBoard[r][column] === BLANK) {
-                newBoard[r][column] = "X";
+                // Place the player's token based on the turn
+                newBoard[r][column] = is_p1_turn ? PLAYER1 : PLAYER2;
+                setTurn(is_p1_turn ? PLAYER2 : PLAYER1);
                 setCurrentBoard(newBoard);
+
+                // meta
                 console.log("Updated column: " + column + " at row: " + r);
                 return;
             }
         }
+
+        // // Drop the token to the bottom at the given column
+        // // Note: slice is neccessary to trigger react to refresh/update the board
+        // let newBoard = currentBoard.slice();
+        // let symbol = is_p1_turn ? PLAYER1 : PLAYER2;
+        // for (let r = 0; r < newBoard.length; r++) {
+        //     if (newBoard[r][column] === BLANK) {
+        //         // Place the player's token based on the turn
+        //         newBoard[r][column] = symbol;
+        //         setCurrentBoard(newBoard);
+
+        //         // Clear the animation
+        //         if (r > 0) {
+        //             newBoard[r - 1][column] = BLANK;
+        //             setCurrentBoard(newBoard);
+        //         }
+        //     }
+        //     await sleep(1000);
+        // }
+        // setTurn(is_p1_turn ? PLAYER2 : PLAYER1);
     }
 
     return (
