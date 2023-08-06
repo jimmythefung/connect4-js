cf18fb15656958e8241e8fdfee8be28dad217914
Jimmy Fung <jimmythefung@yahoo.com>
2023-08-05 23:00:13 -0400

    add/remove class

--- public/shared.js
+++ public/shared.js
@@ -3,11 +3,11 @@ var mobileNav = document.querySelector(".mobile-nav");
 
 var backdrop = document.querySelector(".backdrop");
 backdrop.addEventListener("click", function(){
-    mobileNav.style.display = "none";
-    backdrop.style.display = "none";
+    mobileNav.classList.remove('open');
+    backdrop.classList.remove('open');
 })
 
 toggleBtn.addEventListener("click", function () {
-    mobileNav.style.display = "block";
-    backdrop.style.display = "block";
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
 });
