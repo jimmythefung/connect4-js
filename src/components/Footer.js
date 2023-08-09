ec694bc9a1019c967d981caff89355fbb141b50c
Jimmy Fung <jimmythefung@yahoo.com>
2023-08-09 03:24:24 -0400

    position fix navbar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,17 +4,14 @@ import Script from "next/script";
 export default function Footer() {
     return (
         <>
-            <div className="footer">
-                <ul className="footer__items">
-                    <li className="footer__item">
-                        <Link href="/">Support</Link>
-                    </li>
-                    <li className="footer__item">
-                        <Link href="/">Terms of Use</Link>
-                    </li>
-                </ul>
-            </div>
-
+            <ul className="footer">
+                <li className="footer__item">
+                    <Link href="/">Support</Link>
+                </li>
+                <li className="footer__item">
+                    <Link href="/">Terms of Use</Link>
+                </li>
+            </ul>
             <Script src="shared.js" />
         </>
     );
